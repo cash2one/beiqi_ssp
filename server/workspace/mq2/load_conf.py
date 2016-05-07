a515a2e93e5b9cca4ca06819881ff4012070eedc
yangwei <yangwei@beiqicloud.com>
2016-05-07 11:02:15 +0800

    * update config

--- server/workspace/mq2/load_conf.py
+++ server/workspace/mq2/load_conf.py
@@ -1,6 +1,5 @@
 #coding:utf-8
 import site, os; site.addsitedir(os.path.dirname(os.path.realpath(__file__))); site.addsitedir(os.path.dirname(os.path.dirname(os.path.realpath(__file__)))); site.addsitedir(os.path.join(os.path.dirname(os.path.dirname(os.path.realpath(__file__))), "common_server"))
-from utils import logger
 from util.stream_config_client import load as conf_load
 from util.convert import resolve_redis_url
 
@@ -28,5 +27,4 @@ for section in (s for s in mq_conf.get_sections() if not s.startswith('_')):
                 ratio,
                 run_or_not
             ))
-            logger.debug('>> {0}'.format(output))
             print output
