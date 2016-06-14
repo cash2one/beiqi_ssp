d530a292b2a1755e3c932b6306abff19c39e2226
yangwei <yangwei@beiqicloud.com>
2016-06-14 20:14:10 +0800

    * change mqtt handler flow

--- server/utest_workspace/utest_api_server/utest_smoke/utest_chat.py
+++ server/utest_workspace/utest_api_server/utest_smoke/utest_chat.py
@@ -11,32 +11,32 @@ from utils.network.mqtt import MQTTClient
 from interfaces.api_server.http_rpc import chat_bcast
 from utest_lib import gen_test_tk
 from util.oem_account_key import APP_SECRET
+from mqtt_server.common.opcode import C_SUB_BEIQI_MSG_BCAST
 
-
-PUB_BEIQI_MSG_BCAST = "BEIQI_MSG_BCAST/{gid}"
-SUB_BEIQI_MSG_BCAST = "BEIQI_MSG_BCAST/#"
-
-
-APIChatMsgBcastTestHdl = None
-
-class MqttInst(MQTTClient):
-    def on_message(self, mqttc, userdata, msg):
-        if APIChatMsgBcastTestHdl:
-            APIChatMsgBcastTestHdl.stop()
-
-GMqttClient = MqttInst()
-GMqttClient.init(MQTT_IP, 1883)
-GMqttClient.subscribe(SUB_BEIQI_MSG_BCAST, None)
-GMqttClient.start()
 
 class APIChatMsgBcastTest(AsyncTestCase):
+    GMqttClient = MQTTClient()
+    GMqttClient.init(MQTT_IP, 1883)
+    GMqttClient.start()
+
+    test_chat_msg_bcast_hdl = None
+
     def test_chat_msg_bcast(self):
+        self.test_chat_msg_bcast_hdl = self
+
         file_type = "3"
         fn = "test_fn.amr"
         ref = "reftest" * 20
 
+        self.bcast_topic = C_SUB_BEIQI_MSG_BCAST.replace("{gid}", "#")
+        print self.bcast_topic
+        self.GMqttClient.subscribe(self.bcast_topic, self.chat_bcast_res)
+        time.sleep(SYNC_WAIT_TIME)
+
         chat_bcast(SERVER_IP, gen_test_tk(), APP_SECRET, file_type, fn, ref)
-        global APIChatMsgBcastTestHdl
-        APIChatMsgBcastTestHdl = self
-        self.wait(timeout=5)
+        self.wait(timeout=SYNC_WAIT_TIME)
+
+    def chat_bcast_res(self, mqttc, userdata, topic, payload):
+        print topic
+        self.test_chat_msg_bcast_hdl.stop()
 
